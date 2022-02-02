2afa8db1fdbb9683f0d1b5b2b672f34f502f3e16
Fatme Ali <96138308+fatmeali97@users.noreply.github.com>
2022-02-02 15:50:25 +0200

    Update script.js

--- Practice/03_07/script.js
+++ Practice/03_07/script.js
@@ -7,3 +7,30 @@
  * - Find an object that has another object inside of it to create a nested object.
  * - Test your objects in the browser console by accessing the entire object and its specific properties.
  */
+ const dog = {
+     name: "Micho",
+     age: 2,
+     features: {
+         breed: "pomeranian",
+         fur: "long fur",
+         color: "light brown"
+     }
+ };
+ var look = dog.features;
+
+ console.log("hey, this is my dog", dog)
+//another way
+ console.log("hey, this is my dog's name", dog["name"])
+ console.log("hey, this is my dog, features:", dog.features)
+ console.log("hey this is my dog's color", dog.features.color)
+ console.log("this is my new variable:", look)
+     //  or
+ let fullinfo = function() {
+     console.log("my dog's name is", dog.name, "his age is", dog.age, "and he is:", dog.features.breed, "with", dog.features.fur,
+         "and", dog.features.color, "color");
+ }
+ let newFunction = function() {
+     console.log("the feature of my dog is", look)
+ }
+ newFunction();
+ fullinfo();
